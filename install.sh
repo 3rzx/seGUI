982c4be4dac7a472e67b498c45f6f7bb16085286
uscc <uscc@localhost.localdomain>
2018-03-23 09:02:45 -0400

    Add installation script

---
+++ install.sh
@@ -0,0 +1,21 @@
+# install dependency
+sudo yum check-update
+sudo yum -y install audit-libs-devel \
+bison \
+bzip2-devel \
+dbus-devel \
+dbus-glib-devel \
+flex \
+flex-devel \
+flex-static \
+glib2-devel \
+libcap-devel \
+libcap-ng-devel \
+pam-devel \
+pcre-devel \
+python-devel \
+setools-devel \
+swig \ 
+redhat-rpm-config \
+selinux-policy-devel \
+policycoreutils-gui 
